52f6af13452a8add563921c1ad95b3dc374f251f
Liliia Baieva <555blv@gmail.com>
2023-03-08 18:59:41 +0100

    add change car info and add print

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,26 @@ public class Main {
    Car secondCar = new Car("Tesla model X", "IA123");
    Car anotherCar = new Car("Tesla model Y", "AA567");
 
+// для вызова метода мы пишем: имяОбъекта.метод(аргументы)
    System.out.println(anotherCar.getInfo());
+
+   myCar.setPlate("DE 123"); // задали номер - раньше его не было, было значение по умолчанию
+   anotherCar.setPlate("DE 567");
+   System.out.println("myCar: " + myCar.getInfo());
+   System.out.println("anotherCar: " + anotherCar.getInfo());
+   System.out.println();
+   System.out.println("Модель anotherCar: " + anotherCar.getModel());
+
+   myCar.setDriverAge(15);
+   System.out.println("myCar.DriverAge = " + myCar.getDriverAge());
+   System.out.println("Минимальный возраст для управления автомобилем = " + Car.getMinimalAge());
+
+   Car.setMinimalAge(14);
+   anotherCar.setDriverAge(15);
+   System.out.println("anotherCar.DriverAge = " + anotherCar.getDriverAge());
+
+
+
 
   }
 
