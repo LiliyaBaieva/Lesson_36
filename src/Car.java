c7f6cd34ab4875941f9f8171c8a63c6970762107
Liliia Baieva <555blv@gmail.com>
2023-03-08 15:27:58 +0100

    add class Car()

---
+++ src/Car.java
@@ -0,0 +1,36 @@
+public class Car {
+  private double enginePower;
+  private String model;
+  private String plate;
+  private static int minimalAge = 18; // нинкак не связан с конкретной машины, один для всех
+  private int driverAge;
+
+  public Car(){
+    this.model = "UNKNOWN MODEL";
+    this.plate = "NO PLATE";
+  }
+  public Car(String model){
+    this.model = model;   // this в любом методе - обращение к ТЕКУЩЕМУ объекту
+  }
+
+  public Car(String model, String plate){
+    this.model = model;
+    this.plate = plate;
+  }
+  // Конструкторов может быть несколько -- а может быть только один. Решать вам.
+
+  public Car(String model, String plate, double power){
+    this.model = model;
+    this.plate = plate;
+    enginePower = power;
+  }
+
+  public String getInfo(){
+    return String.format("%s: %s, (%f)", model, plate, enginePower);
+  }
+
+
+
+
+
+}
